22c36ff29d3d655030e3211e3fe1f8884b58f256
sutthiphatchuenban <sutthiphatchuenban@gmail.com>
2025-08-16 01:48:08 +0700

    Update server.js

--- server.js
+++ server.js
@@ -24,10 +24,7 @@ const PORT = process.env.PORT || 3001;
 
 // Security middleware
 app.use(helmet());
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://nnote-beta.vercel.app', 'http://localhost:5174'],
-  credentials: true
-}));
+app.use(cors());
 
 // Rate limiting
 const limiter = rateLimit({
